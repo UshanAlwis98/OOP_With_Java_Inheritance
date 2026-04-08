cb57ff0f7209e83ae477e0cafabd5d3dd7eb046d
ushanAlwis98 <ushanloshitha@gmail.com>
2026-04-08 12:48:30 +0530

    car object created.

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,19 @@ import ChatGPT_Challange_01.BankAccount;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//
+//        BankAccount ba1 = new BankAccount("345876", "Ushan Loshitha", 545);
+//
+//        System.out.println(ba1.getBalance());
+//
+//        System.out.println(ba1.deposit(500));
+//
+//        System.out.println(ba1.withdraw(300));
+//
+//        ba1.displayAccountDetails();
 
-        BankAccount ba1 = new BankAccount("345876", "Ushan Loshitha", 545);
+        Car car = new Car();
+        car.describeCar();
 
-        System.out.println(ba1.getBalance());
-
-        System.out.println(ba1.deposit(500));
-
-        System.out.println(ba1.withdraw(300));
-
-        ba1.displayAccountDetails();
     }
 }
